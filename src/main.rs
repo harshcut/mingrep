89847315500774953ca8024a4640042e01bbb0e9
Harsh Karande <harshskarande@gmail.com>
2021-11-15 16:14:37 +0530

    Parse arguments to search functions

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,18 @@
-fn main() {}
+use std::env;
+use std::process;
+
+use mingrep::Config;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        eprintln!("Parsing Error: {}", err);
+        process::exit(1);
+    });
+
+    if let Err(err) = mingrep::run(config) {
+        eprintln!("Application Error: {}", err);
+        process::exit(1);
+    }
+}
